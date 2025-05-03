b7af4a511f76b04a18291220e1f4e4f96d943f49
Gcferreira2023 <145936902+Gcferreira2023@users.noreply.github.com>
2025-05-03 11:33:11 -0300

    Update useConversationMessages.ts

--- src/hooks/useConversationMessages.ts
+++ src/hooks/useConversationMessages.ts
@@ -1,6 +1,7 @@
 
 import { useAuth } from "@/contexts/AuthContext";
 import { useConversationData } from "./messages/useConversationData";
+import { Message } from "../types/messages"; // Importe a interface centralizada
 
 export const useConversationMessages = (adId?: string, otherId?: string | null) => {
   const { user } = useAuth();
